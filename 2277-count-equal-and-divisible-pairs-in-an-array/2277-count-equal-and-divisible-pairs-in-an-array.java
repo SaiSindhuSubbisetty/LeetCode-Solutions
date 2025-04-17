7a7557e62367959f8ce5502fab6ce6117b1b440f
SaiSindhuSubbisetty <sindhusai443@gmail.com>
2025-04-17 10:01:21 +0530

    Time: 5 ms (11.58%), Space: 42.7 MB (38.8%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
@@ -1,18 +1,34 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public int countPairs(int[] nums, int k) {
-        int n = nums.length;
         int count = 0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i] == nums[j] && (i*j)%k == 0)
-                {
-                    count++;
+
+        // Step 1: Map to group indices by the value of nums[i]
+        Map<Integer, List<Integer>> valueToIndices = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++) {
+            // Group all indices for each value
+            if(!valueToIndices.containsKey(nums[i])){
+                valueToIndices.put(nums[i], new ArrayList<>());
+            }
+            valueToIndices.get(nums[i]).add(i);
+        }
+
+        // Step 2: For each group of equal numbers, check pairs of indices
+        for(List<Integer> indices : valueToIndices.values()){
+            int size = indices.size();
+              // Check all pairs in this group
+            for(int i=0;i<size;i++){
+                for(int j=i+1;j<size;j++){
+                    int index1 = indices.get(i);
+                    int index2 = indices.get(j);
+                    if((index1*index2)%k == 0){
+                        count++;
+                    }
                 }
             }
         }
-        return count;
-        
+         return count;
     }
 }
