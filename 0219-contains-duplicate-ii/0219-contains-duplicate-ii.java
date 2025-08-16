2279237b3e78c7a69c1c32783cc129a6560760f9
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-08-16 19:50:40 +0530

    Time: 21 ms (78.65%), Space: 60.1 MB (22.11%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hmap.containsKey(nums[i]) && i - hmap.get(nums[i]) <= k)
+            {
+                return true;
+            }
+            hmap.put(nums[i], i);
+            
+        }
+        return false;
+        
+    }
+}
