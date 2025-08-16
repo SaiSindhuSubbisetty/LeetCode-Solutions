5855fbd676d7544f0fb21ea0d6f38f84d0424680
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-08-16 20:33:55 +0530

    Time: 2 ms (73.95%), Space: 53.2 MB (49.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,7 +3,8 @@ class Solution {
         //Best Solution: Boyer-Moore Voting Algorithm (Optimal)
         int count = 0;
         int candidate = 0;
-        for(int num : nums){
+        for(int num : nums)
+        {
             if(count == 0)
             {
                 candidate = num;
