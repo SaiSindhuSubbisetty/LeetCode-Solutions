bfa0878a95873511b50aa09286a7883070991b1f
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-08-21 20:23:05 +0530

    Time: 140 ms (6.13%), Space: 43.2 MB (76.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,11 +2,17 @@ import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
-        
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }  
+        for(int i:count)
+        {
+            if(i!=0)
+                return false;
+        }
+        return true;
     }
 }
