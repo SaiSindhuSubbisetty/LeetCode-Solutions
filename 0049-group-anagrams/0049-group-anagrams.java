b4084b631c07cdad806b9ca4401af5122ab80cdc
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-08-21 21:00:18 +0530

    Time: 20 ms (14.83%), Space: 48.2 MB (27.55%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            // Build a unique key like "1a1e1t" for "eat"
+            StringBuilder sb = new StringBuilder();
+            for (int num : count) {
+                sb.append("#").append(num);
+            }
+            String key = sb.toString();
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
