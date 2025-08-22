4e1fb6e4ca0962080908b2a1363981f17da45dcf
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-08-22 05:32:07 +0530

    Time: 2 ms (96.63%), Space: 41.8 MB (66.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack.*;
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(Character c: s.toCharArray())
+        {
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+                if((top == '(' && c != ')') ||
+                    (top == '{' && c != '}') ||
+                    (top == '[' && c != ']')
+                ){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
+//TC:0(n)
+//Sc:0(n)
