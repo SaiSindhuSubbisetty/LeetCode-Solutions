69c261f437d34392dfdd8e8d1c043f54bfdbe6b6
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-09-04 13:22:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (61.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,7 +4,8 @@ class Solution {
         int p1 = m-1;
         int p2 = n-1;
         int p3 = nums1.length-1;
-        while(p3 >= 0){
+        while(p3 >= 0)
+        {
             int elem1,elem2;
             elem1 = (p1>=0)?nums1[p1]:Integer.MIN_VALUE;
             elem2 = (p2>=0)?nums2[p2]:Integer.MIN_VALUE;
@@ -19,5 +20,9 @@ class Solution {
             }
             p3--;
         }
+
     }
 }
+
+//TC : 0(m+n)
+//SC : 0(1)
