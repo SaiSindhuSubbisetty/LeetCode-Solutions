f8a65eb1089b15318c2fd8066ab352efea5b92d2
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-09-06 20:47:18 +0530

    Time: 6 ms (42.47%), Space: 43.1 MB (82.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,16 +2,18 @@ import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        int[] count = new int[26];
+        int[] count=new int[26];
         for(int i=0;i<s.length();i++)
         {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
-        }  
-        for(int i:count)
+        }
+        for(int c:count)
         {
-            if(i!=0)
+            if(c!=0)
+            {
                 return false;
+            }
         }
         return true;
     }
