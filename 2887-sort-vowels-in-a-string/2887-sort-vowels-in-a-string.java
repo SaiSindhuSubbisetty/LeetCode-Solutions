91c35ef2a219701e3e13ec002064251d58584cb3
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-09-11 20:20:51 +0530

    Time: 22 ms (84.24%), Space: 45.8 MB (87.03%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,27 +1,32 @@
 import java.util.*;
 class Solution {
     public String sortVowels(String s) {
-        String vowels  = "aeiouAEIOU";
+       String vowel = "aeiouAEIOU";
 
-        List<Character> extracted = new ArrayList<>();
-        for(char ch : s.toCharArray()){
-            if(vowels.indexOf(ch) != -1){
-                extracted.add(ch);
+       List<Character> extracted = new ArrayList<>();
+       int[] count = new int[128];
+       for(char ch: s.toCharArray()){
+         if(vowel.indexOf(ch) != -1){
+            count[ch]++;
+         }
+       }
+       StringBuilder result = new StringBuilder();
+       int vowelPtr = 0;
+       for(char ch:s.toCharArray()){
+          if(vowel.indexOf(ch) != -1){
+            while(vowelPtr < 128 && count[vowelPtr] == 0){
+                vowelPtr++;
             }
-        }
-        Collections.sort(extracted);
+            result.append((char) vowelPtr);
+            count[vowelPtr]--;
+          }
+          else{
+            result.append(ch);
+          }
 
-        StringBuilder result = new StringBuilder();
-        int j = 0;
-        for(char ch : s.toCharArray()){
-            if(vowels.indexOf(ch)!=-1){
-                result.append(extracted.get(j++));
-            }
-            else{
-                result.append(ch);
-            }
-        }
-        return result.toString();
-        
+
+       }
+       return result.toString();
+
     }
 }
