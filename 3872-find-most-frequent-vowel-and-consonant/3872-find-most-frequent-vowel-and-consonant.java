51aaf02f3ce87b223b0af6a49e83cec325b9e53d
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-09-13 20:51:39 +0530

    Time: 3 ms (60.85%), Space: 42.9 MB (24.31%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+        int maxVowel = 0;
+        int maxConsonant = 0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                char ch = (char) ('a'+i);
+                if(vowels.contains(ch)){
+                    maxVowel = Math.max(maxVowel,freq[i]);
+                }
+                else{
+
+                    maxConsonant = Math.max(maxConsonant,freq[i]);
+                
+                }
+            }
+        }
+        
+        return maxVowel+maxConsonant;
+    }
+}
