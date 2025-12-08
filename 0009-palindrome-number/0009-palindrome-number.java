24463e12678ecd3bf2df68c4a19775b85031ce01
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2025-12-08 21:10:21 +0530

    Time: 5 ms (71.86%), Space: 45.5 MB (55.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        int left = 0, right = s.length()-1;
-        while(left < right)
+        if(x < 0 || (x %10 ==0 && x!=0))
         {
-            if(s.charAt(left) != s.charAt(right))
-            {
-                return false;
-            }
-            left++;
-            right--;
-        } 
-        return true;
+            return false;
+        }
+        int reversed = 0;
+        while(x > reversed)
+        {
+            reversed = reversed * 10 +x%10;
+            x /= 10;
+        }
+        return x == reversed || x == reversed/10;
     }
 }
