230562946e2ed4fc793d6d4f1035ffafa122d246
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-01-07 21:07:32 +0530

    Time: 3 ms (51.73%), Space: 46.8 MB (24.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,42 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+       int N = 9;
+       HashSet<Character>[] rows = new HashSet[N];
+       HashSet<Character>[] cols = new HashSet[N];
+       HashSet<Character>[] boxs = new HashSet[N];
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
+       for(int r=0;r<N;r++)
+       {
+         rows[r] = new HashSet<Character>();
+         cols[r] = new HashSet<Character>();
+         boxs[r] = new HashSet<Character>();
+       }
 
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
-                        return false;
-                    }
-
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
-                }
+       for(int r=0;r<N;r++)
+       {
+         for(int c=0;c<N;c++)
+         {
+            char val = board[r][c];
+            if(val == '.'){
+                continue;
             }
-        }
-        return true;
+            if(rows[r].contains(val)){
+                return false;
+            }
+            rows[r].add(val);
+
+            if(cols[c].contains(val)){
+                return false;
+            }
+            cols[c].add(val);
+
+            int idx = (r/3)*3+c/3;
+            if(boxs[idx].contains(val)){
+                return false;
+            }
+            boxs[idx].add(val);
+         }
+       }
+       return true;
     }
 }
