64e83b3d587003eee1d7e9a48eaf2cd8101f2da9
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-04-01 10:34:12 +0530

    Time: 20 ms (14.41%), Space: 50.4 MB (17.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,34 +1,35 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) {
+        if(strs.length == 0)
+        {
             return new ArrayList<>();
         }
-
-        HashMap<String, List<String>> ansMap = new HashMap<>();
+        
+        HashMap<String,List> hmap = new HashMap<>();
         int[] count = new int[26];
 
-        for (String s : strs) {
-            Arrays.fill(count, 0);
-
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
+        for(String s : strs){
+            Arrays.fill(count,0);
+            for(char c : s.toCharArray())
+            {
+                count[c-'a']++;
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
+            StringBuilder sb  = new StringBuilder();
+            for(int i=0;i<26;i++)
+            {
                 sb.append("#");
                 sb.append(count[i]);
             }
 
             String key = sb.toString();
 
-            if (!ansMap.containsKey(key)) {
-                ansMap.put(key, new ArrayList<>());
+            if(!hmap.containsKey(key))
+            {
+                hmap.put(key, new ArrayList());
             }
-
-            ansMap.get(key).add(s);
+            hmap.get(key).add(s);
         }
-
-        return new ArrayList<>(ansMap.values());
+        return new ArrayList(hmap.values());
     }
 }
