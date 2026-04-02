49254fe2c4bf13dd3ef5759fff1ddf06af53c8d6
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-04-02 08:02:16 +0530

    Time: 3 ms (52.68%), Space: 46.2 MB (88.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,46 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-       int N = 9;
-       HashSet<Character>[] rows = new HashSet[N];
-       HashSet<Character>[] cols = new HashSet[N];
-       HashSet<Character>[] boxs = new HashSet[N];
+        int N = 9;
 
-       for(int r=0;r<N;r++)
-       {
-         rows[r] = new HashSet<Character>();
-         cols[r] = new HashSet<Character>();
-         boxs[r] = new HashSet<Character>();
-       }
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
 
-       for(int r=0;r<N;r++)
-       {
-         for(int c=0;c<N;c++)
-         {
-            char val = board[r][c];
-            if(val == '.'){
-                continue;
-            }
-            if(rows[r].contains(val)){
-                return false;
-            }
-            rows[r].add(val);
+        for(int r=0;r<N;r++)
+        {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
+        
+        for(int r=0;r<N;r++)
+        {
+            for(int c=0;c<N;c++)
+            {
+                char val = board[r][c];
+                if(val == '.')
+                {
+                    continue;
+                }
 
-            if(cols[c].contains(val)){
-                return false;
-            }
-            cols[c].add(val);
+                if(rows[r].contains(val)){
+                    return false;
+                }
+                rows[r].add(val);
 
-            int idx = (r/3)*3+c/3;
-            if(boxs[idx].contains(val)){
-                return false;
+                if(cols[c].contains(val)){
+                    return false;
+                }
+                cols[c].add(val);
+
+                int idx = (r/3)*3+c/3;
+                if(boxes[idx].contains(val))
+                {
+                    return false;
+                }
+                boxes[idx].add(val);
             }
-            boxs[idx].add(val);
-         }
-       }
-       return true;
+        }
+        return true;
     }
 }
