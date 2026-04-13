89cfd8151fa1726f57999398edbabd30cb80d91b
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-04-13 11:42:09 +0530

    Time: 3 ms (87.7%), Space: 43.2 MB (62.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,28 @@
-import java.util.Stack.*;
-import java.util.*;
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(Character c : s.toCharArray())
-        {
-            if(c == '(' || c=='{' || c=='['){
-                st.push(c);
-            }
-            else{
-                if(st.isEmpty()) return false;
-                char top = st.pop();
-                if((top == '(' && c != ')') || 
-                   (top == '{' && c != '}') ||
-                   (top == '[' && c != ']')
-                ){
-                    return false;
+        //Stack only
+        //revrese approach 
 
-                }
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(')
+            {
+                st.push(')');
+            }
+            else if(c == '{')
+            {
+                st.push('}');
+            }
+            else if(c == '[')
+            {
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.pop() != c)
+            {
+                return false;
             }
         }
         return st.isEmpty();
+        
     }
 }
-
-//TC:0(n)
-//Sc:0(n)
