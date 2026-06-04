847395e36ce331e56fd1a355713159bde8207643
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-06-04 10:04:31 +0530

    Time: 132 ms (64.94%), Space: 42.8 MB (74.35%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,11 +2,12 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
-        for(int i=0;i<rows;i++)
+        for(int r=0;r<rows;r++)
         {
-            for(int j=0;j<cols;j++)
+            for(int c=0;c<cols;c++)
             {
-                if(board[i][j] ==word.charAt(0) && helper(board,word,i,j,0)){
+                if(board[r][c] == word.charAt(0) && helper(board,word,r,c,0))
+                {
                     return true;
                 }
             }
@@ -14,24 +15,25 @@ class Solution {
         return false;
         
     }
-    private boolean helper(char[][] board,String word,int i,int j, int count)
+
+    private boolean helper(char[][] board,String word,int i,int j,int count)
     {
         if(count == word.length())
         {
             return true;
         }
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(count)){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != word.charAt(count))
+        {
             return false;
         }
         char temp = board[i][j];
         board[i][j] = ' ';
         boolean found = helper(board,word,i+1,j,count+1)
-                    || helper(board,word,i-1,j,count+1)
-                    || helper(board,word,i,j+1,count+1)
-                    || helper(board,word,i,j-1,count+1);
-        board[i][j] = temp;
-        return found;            
+                       ||  helper(board,word,i-1,j,count+1)
+                       ||  helper(board,word,i,j+1,count+1)
+                       ||  helper(board,word,i,j-1,count+1);
+        board[i][j] = temp;               
 
-    }
+        return found;               
+    } 
 }
-
