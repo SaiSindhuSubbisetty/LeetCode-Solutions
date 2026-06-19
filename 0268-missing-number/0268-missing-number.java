50b891b6186734264783d802b603e97a662bfed3
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-06-19 10:38:35 +0530

    Time: 0 ms (100%), Space: 47.6 MB (19.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,28 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        //cyclic sort;
-        int i=0;
-        while(i<nums.length){
-            int correct = nums[i];
-            if(nums[i] < nums.length && nums[i] != nums[correct])
-            {
-                swap(nums,i,correct);
-            }
-            else{
-                i++;
-            }
+        int xOr = 0;
+        for(int i=0;i<=nums.length;i++)
+        {
+            xOr = xOr ^ i;
         }
-        //searching and finding
-        for(int index=0;index<nums.length;index++){
-            if(nums[index] != index){
-                return index;
-            }
+        for(int i=0;i<nums.length;i++)
+        {
+            xOr = xOr ^ nums[i];
         }
-        return nums.length;
-    }
-    public void swap(int[] nums,int start,int end){
-        int temp = nums[start];
-        nums[start] =nums[end];
-        nums[end] = temp;
+        return xOr;
     }
 }
