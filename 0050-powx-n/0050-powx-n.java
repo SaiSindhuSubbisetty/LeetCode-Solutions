59a23e73ef427ea8c10e538a2ec33077512051ec
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-06-26 10:48:21 +0530

    Time: 0 ms (100%), Space: 47.6 MB (85.45%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        long N = n;
+        if(N < 0)
+        {
+            N = -N;
+            x = 1/x;
+        }
+        double result = 1;
+        double currentProduct = x;
+        while(N > 0)
+        {
+            if(N%2 == 1)
+            {
+                result *= currentProduct;
+            }
+            currentProduct *= currentProduct;
+            N /= 2;
+        }
+        return result;
+    }
+}
