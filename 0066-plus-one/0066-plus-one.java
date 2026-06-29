4e977d5adcc91cb716330a16f8ddf0fe939c2e88
Sai Sindhu Subbisetty <sindhusai443@gmail.com>
2026-06-29 09:41:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (60.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for(int i=n-1;i>=0;i--){
-            if(digits[i] == 9){
-                digits[i] = 0;
-            }
-            else{
-                digits[i] += 1;
+        for(int i= digits.length-1;i>=0;i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++;
                 return digits;
             }
+            digits[i] = 0;
         }
-        int[] newNumber = new int[n+1];
-        newNumber[0] = 1;
-        return newNumber;
+        int[] newDigits = new int[digits.length+1];
+        newDigits[0] = 1;
+        return newDigits;
         
     }
 }
